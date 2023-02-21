3a7755c2f0f39f7bf2d0972a72818c6c5a98e6c9
djo1975 <mladanilic1975@gmail.com>
2023-02-21 22:33:23 +0100

    jebaga1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,5 @@
-import './bootstrap-progressbar.js';
 import './bootstrap.js';
+import './bootstrap-progressbar.js';
 import './custom.js';
 import './jquery.counterup.js';
 import './jquery.fancybox.pack.js';
@@ -7,3 +7,12 @@ import './jquery.mixitup.js';
 import './slick.js';
 import './waypoints.js';
 import './wow.js';
+
+import '../css/font-awesome.css';
+import '../css/bootstrap.css';
+import '../css/slick.css';
+import '../css/jquery.fancybox.css';
+import '../css/animate.css';
+import '../css/bootstrap-progressbar-3.3.4.css';
+import '../css/theme-color/default-theme.css';
+import '../../style.css';
