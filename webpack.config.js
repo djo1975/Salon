3a5fa2b6885dd27522f509df9d8efee98efb262d
djo1975 <mladanilic1975@gmail.com>
2023-02-21 21:05:58 +0100

    jebaga1

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle[contenthash].js',
+  publicPath: '/',
   },
   devtool: 'source-map',
   devServer: {
